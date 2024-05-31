282f64c036f6c45019f98bb5709a0208cc88f976
Zach_Mason <zmason04@gmail.com>
2024-05-31 13:42:59 -0400

    minor changes to files

--- EmployeeSQL/tables_query.sql
+++ EmployeeSQL/tables_query.sql
@@ -6,20 +6,20 @@ DROP TABLE IF EXISTS Titles;
 DROP TABLE IF EXISTS Departments;
 
 CREATE TABLE Departments (
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
   	dept_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE Titles (
-	title_id VARCHAR NOT NULL,
+	title_id VARCHAR(10) NOT NULL,
 	title VARCHAR(30),
 	PRIMARY KEY (title_id)
 );
 
 CREATE TABLE Employees (
 	emp_no INTEGER NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
@@ -31,14 +31,14 @@ CREATE TABLE Employees (
 
 CREATE TABLE Dept_Emp (
 	emp_no INTEGER NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
 );
 
 
 CREATE TABLE Dept_Manager (
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
   	emp_no INTEGER NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
